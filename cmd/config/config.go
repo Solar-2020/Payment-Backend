2d48dfba3b2b1c53aaf8cc2f1bd89b23a530bdfd
veinskiy.v <veinskiy.v@wildberries.ru>
2020-11-17 14:53:41 +0300

    add success and fail url config

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -12,8 +12,8 @@ type config struct {
 	DomainName                      string `envconfig:"DOMAIN_NAME" default:"solar.ru"` //for static file prefix
 	ServerSecret                    string `envconfig:"SERVER_SECRET" default:"Basic secret"`
 	MoneyClientID                   string `envconfig:"MONEY_CLIENT_ID"`
-	MoneySuccessURL                 string `envconfig:"MONEY_SUCCESS_URL"`
-	MoneyFailURL                    string `envconfig:"MONEY_FAIL_URL"`
+	MoneySuccessURL                 string `envconfig:"MONEY_SUCCESS_URL" default:"https://develop.pay-together.ru/pay/success"`
+	MoneyFailURL                    string `envconfig:"MONEY_FAIL_URL" default:"https://develop.pay-together.ru/pay/error"`
 	AuthServiceHost                 string `envconfig:"AUTH_SERVICE_HOST" default:"develop.pay-together.ru"`
 	GroupServiceHost                string `envconfig:"GROUP_SERVICE_HOST" default:"develop.pay-together.ru"`
 	AccountServiceHost              string `envconfig:"ACCOUNT_SERVICE_HOST" default:"develop.pay-together.ru"`
