5e8da8b28e93738a099b4228d5d11554e25f1a50
Andrey Kochnov <a.kochnov@corp.mail.ru>
2020-11-24 22:26:52 +0300

    Add payment methods

--- internal/storages/paymentStorage/storage.go
+++ internal/storages/paymentStorage/storage.go
@@ -1,7 +1,10 @@
 package paymentStorage
 
 import (
+	"bytes"
 	"database/sql"
+	"errors"
+	"fmt"
 	"github.com/Solar-2020/Payment-Backend/pkg/models"
 	"strconv"
 	"strings"
@@ -10,6 +13,15 @@ import (
 const (
 	queryReturningID = "RETURNING id;"
 )
+
+var paymentMethodsSchema = map[models.PaymentType]struct{
+	tableName string
+	cols []string
+}{
+	models.YoomoneyType: {"yoomoney_account", []string{"account_number"}},
+	models.PhoneType: {"phone_payment", []string{"phone_number"}},
+	models.CardType: {"bank_card", []string{"bank_title", "phone_number", "card_number"}},
+}
 
 type storage struct {
 	db *sql.DB
@@ -42,8 +54,31 @@ func (s *storage) InsertPayments(payments []models.Payment, createBy, groupID, p
 		sqlQuery = strings.Replace(sqlQuery, "?", "$"+strconv.Itoa(i), 1)
 	}
 
-	_, err = s.db.Exec(sqlQuery, params...)
+	tx, err := s.db.Begin()
+	if err != nil {
+		return
+	}
 
+	rows, err := tx.Query(sqlQuery, params...)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	i := int(0)
+	for rows.Next() {
+		rows.Scan(&payments[i].ID)
+		i += 1
+	}
+
+	for _, payment := range payments {
+		err = s.insertMethod(tx, payment.Methods, payment.ID, createBy)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	err = tx.Commit()
 	return
 }
 
@@ -81,6 +116,11 @@ func (s *storage) SelectPaymentsByPostsIDs(postIDs []int) (payments []models.Pay
 		if err != nil {
 			return
 		}
+		methods, err := s.selectMethods(tempPayment.ID)
+		if err != nil {
+			return payments, err
+		}
+		tempPayment.Methods = methods
 		payments = append(payments, tempPayment)
 	}
 
@@ -101,6 +141,107 @@ func (s *storage) SelectPayment(paymentID int) (payment models.Payment, err erro
 
 	err = s.db.QueryRow(sqlQuery, paymentID).Scan(&payment.ID, &payment.TotalCost, &payment.PaymentAccount, &payment.GroupID, &payment.PostID)
 
+	if err != nil {
+		return
+	}
+	methods, err := s.selectMethods(paymentID)
+	if err != nil {
+		return
+	}
+	payment.Methods = methods
+
+	return
+}
+
+
+func (s *storage) insertMethod(tx *sql.Tx, methods []models.PaymentMethod, paymentID int, owner int) (err error) {
+	if len(methods) == 0 {
+		return
+	}
+
+	paramsList := map[models.PaymentType][]interface{}{
+		models.CardType: make([]interface{}, 0),
+		models.PhoneType: make([]interface{}, 0),
+		models.YoomoneyType: make([]interface{}, 0),
+	}
+
+	const sqlQueryTemplate = `
+	INSERT INTO %s(payment_id, owner, %s)
+	VALUES `
+
+	for _, method := range methods {
+		params := paramsList[method.Type]
+		params = append(params, paymentID, owner)
+		switch method.Type{
+		case models.YoomoneyType:
+			params = append(params, method.AccountNumber)
+		case models.CardType:
+			params = append(params, method.BankName, method.PhoneNumber, method.CardNumber)
+		case models.PhoneType:
+			params = append(params, method.PhoneNumber)
+		default:
+			return errors.New("bad payment type: " + string(method.Type))
+		}
+		paramsList[method.Type] = params
+	}
+
+	for payType, params := range paramsList {
+		if len(params) == 0 {
+			continue
+		}
+		paramsCount := 2 + len(paymentMethodsSchema[payType].cols)
+
+		sqlQuery := fmt.Sprintf(sqlQueryTemplate,
+			paymentMethodsSchema[payType].tableName,
+			strings.Join(paymentMethodsSchema[payType].cols, ", "),
+		)
+
+		sqlQuery = CreatePacketQuery(sqlQuery, paramsCount, len(params)/paramsCount, 0)
+		_, err = tx.Exec(sqlQuery, params...)
+		if err != nil {
+			return err
+		}
+	}
+	return
+
+}
+
+func (s *storage) selectMethods(paymentID int) (methods []models.PaymentMethod, err error) {
+	const sqlQueryTemplate = `SELECT owner, payment_id, %s FROM %s WHERE payment_id = $1`
+
+	for payType, schema := range paymentMethodsSchema {
+		sqlQuery := fmt.Sprintf(sqlQueryTemplate,
+			strings.Join(schema.cols, ", "),
+			schema.tableName,
+		)
+
+		rows, err := s.db.Query(sqlQuery, paymentID)
+		if err != nil {
+			return methods, err
+		}
+		for rows.Next(){
+			var tempMethod models.PaymentMethod
+			switch payType {
+			case models.YoomoneyType:
+				err = rows.Scan(&tempMethod.Owner,&tempMethod.PaymentID, &tempMethod.AccountNumber)
+				tempMethod.Type = models.YoomoneyType
+			case models.CardType:
+				err = rows.Scan(&tempMethod.Owner, &tempMethod.PaymentID, &tempMethod.BankName,
+					&tempMethod.PhoneNumber, &tempMethod.CardNumber)
+				tempMethod.Type = models.CardType
+			case models.PhoneType:
+				err = rows.Scan(&tempMethod.Owner,&tempMethod.PaymentID, &tempMethod.PhoneNumber)
+				tempMethod.Type = models.PhoneType
+			default:
+				err = errors.New("bad payment type: " + string(payType))
+				return methods, err
+			}
+			if err != nil {
+				return methods, err
+			}
+			methods = append(methods, tempMethod)
+		}
+	}
 	return
 }
 
@@ -130,4 +271,32 @@ func (s *storage) createInsertQuery(sliceLen int, structLen int) (query string) 
 	query = strings.TrimRight(query, ",")
 
 	return
+}
+
+
+// https://github.com/go-park-mail-ru/2019_2_Next_Level/blob/master/pkg/sqlTools/genQuery.go
+func CreatePacketQuery(prefix string, batchSize int, batchCount int, initIdx int, postfix ...string) string {
+	pack := make([]string, 0, batchCount)
+	batch := make([]string, 0, batchSize)
+
+	for i := 0; i < batchCount; i++ {
+		for j := 1; j <= batchSize; j++ {
+			batch = append(batch, "$"+strconv.Itoa(batchSize*i + j + initIdx))
+		}
+		pack = append(pack, "(" + strings.Join(batch, ", ") + ")" )
+		batch = batch[:0]
+	}
+
+	var res bytes.Buffer
+	res.WriteString(prefix)
+	if prefix[len(prefix)-1] != ' ' {
+		res.WriteString(" ")
+	}
+	res.WriteString(strings.Join(pack, ", "))
+	if len(postfix) > 0 {
+		res.WriteString(" ")
+		res.WriteString(strings.Join(postfix, " "))
+	}
+	res.WriteString(";")
+	return res.String()
 }
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -14,6 +14,42 @@ type Payment struct {
 	GroupID        int             `json:"groupID"`
 	PostID         int             `json:"postID"`
 	CreateBy       int             `json:"createBy"`
-	TotalCost      decimal.Decimal `json:"totalCost"`
+	TotalCost      decimal.Decimal `json:"paymentValue"`
 	PaymentAccount string          `json:"paymentAccount"`
+	Methods []PaymentMethod		   `json:"methods"`
+	//PaymentArrays
+}
+
+//type Payment
+
+type PaymentType string
+
+const (
+	PhoneType    PaymentType = "phone"
+	CardType     PaymentType = "card"
+	YoomoneyType PaymentType = "yoomoney"
+)
+
+type PhonePayment struct {
+	PhoneNumber string `json:"phoneNumber,omitempty"`
+}
+
+type CardPayment struct {
+	BankName string `json:"bankName,omitempty"`
+	CardNumber string `json:"cardNumber,omitempty"`
+	PhonePayment
+}
+
+type YoomoneyPayment struct {
+	AccountNumber string `json:"yoomoneyAccount,omitempty"`
+}
+
+type PaymentMethod struct {
+	ID int	`json:"id,omitempty"`
+	Owner int	`json:"owner"`
+	Type PaymentType `json:"type"`
+	PaymentID int `json:"-"`
+
+	CardPayment
+	YoomoneyPayment
 }
